16ebbdd8c71176a3335ee1b5eb09ff5542cb2a87
Leonardo Buta <leonardo.lbuta@gmail.com>
2022-11-03 10:53:56 -0300

    Commit aula 1

---
+++ Aula01/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Digite o seu nome:");
+
+string? nome = Console.ReadLine();
+
+Console.WriteLine("Digite a sua idade:");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o seu peso:");
+
+double peso = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Apresentação:");
+Console.WriteLine("Seu nome é: " + nome);
+Console.WriteLine("Sua idade é: " + idade);
+Console.WriteLine("Seu peso é: " + peso);
+
+
+// string nome = "Leonardo";
+
+// int idade = 26;
+
+// double peso = 61.50;
+
+// Console.WriteLine("Apresentação:");
+// Console.WriteLine("Seu nome é: " + nome);
+// Console.WriteLine("Sua idade é: " + idade);
+// Console.WriteLine("Seu peso é: " + peso);
