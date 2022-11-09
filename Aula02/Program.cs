9b6b94399c14fd70f60756f8ba14cbfbc62c2db7
Leonardo Buta <leonardo.lbuta@gmail.com>
2022-11-09 11:13:58 -0300

    Adicionado projeto da aula 2

---
+++ Aula02/Program.cs
@@ -0,0 +1,67 @@
+﻿
+// For
+// While
+// Do-While
+
+int acumulador = 0;
+int numero = 1;
+
+// for (int i = 1; i <= 10; i++)
+
+while (numero != 0)
+{
+    numero = Convert.ToInt32(Console.ReadLine());
+    acumulador = acumulador + numero;
+}
+
+Console.WriteLine($"A soma dos números é: {acumulador}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Digite um número:");
+// int numero = Convert.ToInt32(Console.ReadLine());
+
+// // i = i + 1;
+
+// // double nota1
+// // double nota2
+
+// for (int i = 1; i <= 10; i++)
+// {
+//     Console.WriteLine($"{numero} X {i} = {numero * i}");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine($"2 X 1 = {2 * 1}");
+// Console.WriteLine($"2 X 2 = {2 * 2}");
+// Console.WriteLine($"2 X 3 = {2 * 3}");
+// Console.WriteLine($"2 X 4 = {2 * 4}");
+// Console.WriteLine($"2 X 5 = {2 * 5}");
+// Console.WriteLine($"2 X 6 = {2 * 6}");
